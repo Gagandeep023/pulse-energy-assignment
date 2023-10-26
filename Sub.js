12516aef704f00482f243326656aadaa322e77bb
Gagandeep Bhatia <gagan@Gagandeeps-MacBook-Air.local>
2023-10-27 02:37:09 +0530

    Adding all the subscribe data into db

--- Sub.js
+++ Sub.js
@@ -1,12 +1,10 @@
 const mqtt = require("mqtt");
+const uuid = require("uuid");
+const { MeterValueRecords } = require("./models");
 
 var client = mqtt.connect("mqtt://broker.hivemq.com");
 
-
-
-client.on("connect",function()
-
-{
+client.on("connect", function () {
 
     client.subscribe("GaganPulseEnergy");
 
@@ -15,12 +13,44 @@ client.on("connect",function()
 });
 
 
+client.on('message', async (topic, message) => {
+    const meterValueString = message.toString();
 
-client.on("message",function(topic, message){
+    const jsonStartIndex = meterValueString.indexOf('{');
+    let chargePointId;
+    let Payload;
+    if (jsonStartIndex !== -1) {
+        chargePointId = meterValueString.slice(0, jsonStartIndex -1);
+        Payload = meterValueString.slice(jsonStartIndex);
+    } else {
+      console.error('No payload found in the input string.');
+    }
 
-    console.log(message.toString());
+    console.log(chargePointId, Payload);
+    const uuidToken = uuid.v4();
+    console.log(uuidToken);
+    const newRecords = new MeterValueRecords({
+        uuid: uuidToken,
+        charge_point_id: chargePointId,
+        payload: Payload,
+    });
 
-});
+    try {
+
+        newRecords.save()
+            .then(() => {
+                console.log('success')
+            }).catch(error => {
+                console.log(error)
+            })
+
+    } catch (err) {
+
+        res.json({ success: false, msg: err });
+
+    }
+    
+})
 
 
 
